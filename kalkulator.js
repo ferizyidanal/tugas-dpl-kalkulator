811bdc1b6c27fbf3b5394b34621eb4ffa287bd33
ferizyidanal <118708728+ferizyidanal@users.noreply.github.com>
2022-12-02 09:37:03 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -1,10 +1,10 @@
 const readline = require('readline-sync')
 console.log('1.tambah,2.kurang,3.kali,4.bagi ')
-var fungsi = readline.question('pilih fungsi= ') 
+var fungsi = readline.questionInt('pilih fungsi= ') 
 
 if(fungsi == 1 ){
-var angka1 = readline.question("MASUKAN NOMOR= ")
-var angka2 = readline.question("MASUKAN NOMOR= ") 
+var angka1 = readline.questionInt("MASUKAN NOMOR= ")
+var angka2 = readline.questionInt("MASUKAN NOMOR= ") 
 var a1 = angka1;
 var a2 = angka2;
 var hasil = Number(a1) + Number(a2);
@@ -21,8 +21,8 @@ if(fungsi == 2 ){
     }
 
 if(fungsi == 3 ){
-        var angka1 = readline.question("MASUKAN NOMOR= ")
-        var angka2 = readline.question("MASUKAN NOMOR= ") 
+        var angka1 = readline.questionInt("MASUKAN NOMOR= ")
+        var angka2 = readline.questionInt("MASUKAN NOMOR= ") 
         var a1 = angka1;
         var a2 = angka2;
         var hasil = Number(a1) * Number(a2);
